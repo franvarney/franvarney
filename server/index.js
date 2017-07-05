bb99ab819967b6fee825347def308ba4f5bf4e60
Fran Varney <fran.varn@gmail.com>
2017-07-05 16:18:03 -0700

    Connect to Mongo before server start

--- server/index.js
+++ server/index.js
@@ -1,17 +1,41 @@
 const {Server} = require('hapi');
+const Mongoose = require('mongoose');
+const Series = require('run-series');
 
 const Config = require('../config/server');
 const Routes = require('./routes');
 
 const server = new Server();
 
-server.connection({
-  port: parseInt(Config.port, 10)
-});
+function connectToMongo(callback) {
+  Mongoose.connect(Config.mongo, {
+    useMongoClient: true
+  });
 
-server.route(Routes);
+  const Db = Mongoose.connection;
 
-server.start((err) => {
-  if (err) throw err;
-  console.info('Server started at', server.info.uri);
-});
+  Db.on('error', callback);
+  Db.once('open', callback.bind(null, null, `Mongo connected at ${Config.mongo}`));
+}
+
+function startServer(callback) {
+  server.connection({
+    port: parseInt(Config.port, 10)
+  });
+
+  server.route(Routes);
+
+  server.start((err) => {
+    if (err) return callback(err);
+    return callback(null, `Server started at ${server.info.uri}`);
+  });
+}
+
+Series([
+    connectToMongo,
+    startServer
+  ], (err, messages) => {
+    if (err) console.error(err);
+    console.info(messages.join('\r\n'));
+  }
+);
