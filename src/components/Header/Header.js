fbfd7a82014df06b11ba0295875b132097fc4468
Fran Varney <fran.varn@gmail.com>
2017-07-06 11:55:09 -0700

    Add header structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <p>Need For Speed</p>
+        <p>Payback</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
