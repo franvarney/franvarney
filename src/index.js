53ec80e1e4b5233a90bfea20e76268b8ecb14e0c
Fran Varney <fran.varn@gmail.com>
2017-07-05 22:35:03 -0700

    Add browser router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 import App from './containers/App/App';
 
@@ -7,6 +8,11 @@ import './index.css';
 
 import RegisterServiceWorker from './helpers/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter basename="/" forceRefresh={true}>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
 RegisterServiceWorker();
