0553a6966325fb12944b804ce7269b7fdde949be
Fran Varney <fran.varn@gmail.com>
2017-07-05 20:38:42 -0700

    Add database init route

---
+++ server/handlers/setup.js
@@ -0,0 +1,96 @@
+const Faker = require('faker');
+const Series = require('run-series');
+
+const Car = require('../models/car');
+const Track = require('../models/track');
+const User = require('../models/user');
+const Race = require('../models/race');
+
+const IDS = {
+  cars: [],
+  tracks: [],
+  users: []
+};
+
+function createTask(item, next) {
+  return item.save((err) => {
+    if (err) return next(err);
+    return next();
+  });
+}
+
+function generateCars(amount, callback) {
+  const cars = new Array(amount).fill({}).map((car) => {
+    car = new Car({
+      name: `${Faker.random.number({ min: 1980, max: 2015 })} ${Faker.random.words(2)}`,
+      color: Faker.internet.color()
+    });
+
+    IDS.cars.push(car._id);
+    return createTask.bind(null, car);
+  });
+
+  return Series(cars, callback);
+}
+
+function generateTracks(amount, callback) {
+  const tracks = new Array(amount).fill({}).map((track) => {
+    track = new Track({
+      name: Faker.random.words(2),
+      country: Faker.address.countryCode(),
+      fastestTime: Faker.random.number({ min: 50000, max: 60000 })
+    });
+
+    IDS.tracks.push(track._id);
+    return createTask.bind(null, track);
+  });
+
+  return Series(tracks, callback);
+}
+
+function generateUsers(amount, callback) {
+  const users = new Array(amount).fill({}).map((user) => {
+    user = new User({
+      username: Faker.internet.userName(),
+    });
+
+    IDS.users.push(user._id);
+    return createTask.bind(null, user);
+  });
+
+  return Series(users, callback);
+}
+
+function generateRaces(amount, callback) {
+  const participants = new Array(4).fill({}).map(() => {
+    return {
+      user: Faker.random.arrayElement(IDS.users),
+      car: Faker.random.arrayElement(IDS.cars),
+      finishTime: Faker.random.number({ min: 60000, max: 360000 })
+    };
+  });
+
+  const races = new Array(amount).fill({}).map((race) => {
+    race = new Race({
+      track: Faker.random.arrayElement(IDS.tracks),
+      participants
+    });
+
+    return createTask.bind(null, race);
+  });
+
+  return Series(races, callback);
+}
+
+module.exports = function (request, reply) {
+  Series([
+    generateCars.bind(null, request.payload.car),
+    generateTracks.bind(null, request.payload.track),
+    generateUsers.bind(null, request.payload.user),
+    generateRaces.bind(null, request.payload.race)
+  ], (err) => {
+    if (err) return reply(err);
+    console.info('Completed generating and storing sample data');
+    return reply(IDS).code(201);
+  });
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,30 @@
+const Joi = require('joi');
 const KnockKnock = require('knock-knock');
 
+const Setup = require('../handlers/setup');
+
 module.exports = [
+  // server test
   {
     path: '/ping',
     method: 'GET',
     handler: (request, reply) => KnockKnock(reply)
+  },
+
+  // generate and add data to mongo
+  {
+    path: '/setup',
+    method: 'POST',
+    handler: Setup,
+    config: {
+      validate: {
+        payload: Joi.object({
+          car: Joi.number().default(20).min(5),
+          race: Joi.number().default(40).min(5),
+          track: Joi.number().default(10).min(3),
+          user: Joi.number().default(20).min(10)
+        })
+      }
+    }
   }
 ];
