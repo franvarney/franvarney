a2cab3149335c621f47ff345bea93bb4ca6b1a93
Fran Varney <fran.varn@gmail.com>
2017-07-06 11:24:41 -0700

    Add basic 404 page

---
+++ src/containers/NoMatch/NoMatch.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import './NoMatch.css';
+
+class NoMatch extends Component {
+  render() {
+    return (
+      <div className="NoMatch">
+        404: Not Found
+      </div>
+    );
+  }
+}
+
+export default NoMatch;
