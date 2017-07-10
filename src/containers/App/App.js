b98df64d9b649ad19df41e3b64663c3ebe458b58
Fran Varney <fran.varn@gmail.com>
2017-07-09 18:15:03 -0700

    Pull it all together

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,20 +1,26 @@
 import React, {Component} from 'react';
+import {Route, Switch} from 'react-router';
 
-import Logo from '../../assets/images/logo.svg';
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import NoMatch from '../NoMatch/NoMatch';
+import RaceBg1 from '../../assets/images/race_bg_1.png';
+import Races from '../Races/Races';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={Logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="App" style={{ backgroundImage: `url(${RaceBg1})` }}>
+        <Header />
+        <div className="content">
+          <Switch>
+            <Route exact path="/" component={Races} />
+            <Route component={NoMatch} />
+          </Switch>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Footer />
       </div>
     );
   }
