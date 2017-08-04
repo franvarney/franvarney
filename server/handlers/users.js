c85a7f4252e83acdb23acd17096c5bb3cbe86416
Fran Varney <fran.varn@gmail.com>
2017-08-04 16:16:43 -0700

    Add user creation

---
+++ server/handlers/users.js
@@ -0,0 +1,27 @@
+const Boom = require('boom');
+
+const Series = require('run-series');
+const User = require('../models/user');
+
+exports.create = function (request, reply) {
+  Series([
+    function (callback) {
+      User.find({ email: request.payload.email }, (err, user) => {
+        if (err) return callback(Boom.badRequest(err));
+        if (user.length) return callback(Boom.badData('Email already exists'));
+        return callback();
+      });
+    },
+    function (callback) {
+      const user = new User(request.payload);
+
+      user.save((err, user) => {
+        if (err) return callback(Boom.badRequest(err));
+        return callback(null, user);
+      });
+    }
+  ], (err, [,user]) => {
+    if (err) return reply(err);
+    return reply({ id: user.toJSON().id }).code(201);
+  });
+}
