3e43a721ea4c040778b5d4a694fa72c98cea112e
Fran Varney <fran.varn@gmail.com>
2017-08-04 15:30:04 -0700

    Create server with mongo connection

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const {Server} = require('hapi');
+const Mongoose = require('mongoose');
+const Series = require('run-series');
+
+const Config = require('../config');
+
+const server = new Server();
+
+function connectToMongo(callback) {
+  Mongoose.connect(Config.mongo, {
+    useMongoClient: true
+  });
+
+  const Db = Mongoose.connection;
+
+  Db.on('error', callback);
+  Db.once('open', callback.bind(null, null, `Mongo connected at ${Config.mongo}`));
+}
+
+function startServer(callback) {
+  server.connection({
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : null,
+    port: parseInt(Config.port, 10),
+    routes: {
+      cors: true
+    }
+  });
+
+  // routes
+
+  server.start((err) => {
+    if (err) return callback(err);
+    return callback(null, `Server started at ${server.info.uri}`);
+  });
+}
+
+Series([
+    connectToMongo,
+    startServer
+  ], (err, messages) => {
+    if (err) console.error(err);
+    console.info(messages.join('\r\n'));
+  }
+);
