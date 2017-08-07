09f3bbea1609ed263afb4c553b4a346a833c45d9
Fran Varney <fran.varn@gmail.com>
2017-08-06 19:48:09 -0700

    Add reservation functionality

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,13 @@
 const Joi = require('joi');
 
+const Reservations = require('../handlers/reservations');
 const Restaurants = require('../handlers/restaurants');
 const Users = require('../handlers/users');
+
+function createTimesAllowed() {
+  let count = 0;
+  return new Array(48).fill(0).map((item) => count++);
+}
 
 module.exports = [
   {
@@ -19,6 +25,12 @@ module.exports = [
   },
 
   {
+    path: '/api/users',
+    method: 'GET',
+    handler: Users.index
+  },
+
+  {
     path: '/api/restaurants',
     method: 'POST',
     handler: Restaurants.create,
@@ -26,8 +38,8 @@ module.exports = [
       validate: {
         payload: Joi.object().keys({
           name: Joi.string().min(2).max(25).required(),
-          open: Joi.number().min(0).max(2359).required(),
-          close: Joi.number().min(0).max(2359).required(),
+          open: Joi.allow(createTimesAllowed()).required(),
+          close: Joi.allow(createTimesAllowed()).required(),
           tables: Joi.array().min(1).items(Joi.object({
             size: Joi.number().min(1).max(16),
             count: Joi.number().min(1).max(40)
@@ -41,5 +53,36 @@ module.exports = [
     path: '/api/restaurants/{id}',
     method: 'GET',
     handler: Restaurants.get
-  }
+  },
+
+  {
+    path: '/api/restaurants',
+    method: 'GET',
+    handler: Restaurants.index
+  },
+
+  {
+    path: '/api/reservations',
+    method: 'POST',
+    handler: Reservations.create,
+    config: {
+      auth: 'simple',
+      validate: {
+        payload: Joi.object().keys({
+          restaurant: Joi.string().required(),
+          size: Joi.number().min(1).max(16),
+          date: Joi.date().min('now').max(
+            new Date().setFullYear(new Date().getFullYear() + 1)
+          ),
+          time: Joi.allow(createTimesAllowed()).required()
+        })
+      }
+    }
+  },
+
+  {
+    path: '/api/reservations',
+    method: 'GET',
+    handler: Reservations.index
+  },
 ];
