34e151449ec27a8c333be0100c38749b7c62d857
Fran Varney <fran.varn@gmail.com>
2018-04-17 14:22:29 -0700

    Add playhead controller

---
+++ Assets/Scripts/PlayHeadController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayHeadController : MonoBehaviour {
+
+    private GameObject rightWall;
+    private Vector3 endingPosition;
+    private Vector3 start;
+
+    public Vector3 CurrentPosition {
+        get {
+            return transform.position;
+        }
+    }
+
+    private void Awake() {
+        rightWall = GameObject.Find("RightBoundary");
+    }
+
+    public IEnumerator MoveOverSeconds() {
+        float elapsedTime = 0f;
+        Vector3 startingPosition = transform.position;
+        endingPosition = new Vector3(rightWall.transform.position.x, transform.position.y, transform.position.z);
+
+        while (elapsedTime < GameController.Instance.DURATION) {
+            elapsedTime += Time.deltaTime / GameController.Instance.DURATION;
+            transform.position = Vector3.Lerp(startingPosition, endingPosition, elapsedTime);
+            yield return null;
+        }
+
+        transform.position = endingPosition;
+    }
+
+    public void ReturnToOriginalPosition() {
+        // TODO Return to original position or use spawner (leftWall?)
+        // transform.position = start;
+    }
+}
