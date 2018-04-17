5c579c1952e38848ac26563847615c9f104e4448
Fran Varney <fran.varn@gmail.com>
2018-04-16 17:25:23 -0700

    Add load script with game prefab + script

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    private static GameController instance;
+    
+    public static GameController Instance {
+        get {
+            return instance;
+        }
+        set {
+            instance = value;
+        }
+    }
+
+    private void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+    [SerializeField] private GameObject gameController;
+
+    void Awake() {
+        if (GameController.Instance == null)
+            Instantiate(gameController);
+    }
+}
